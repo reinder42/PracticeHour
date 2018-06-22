de405f83f5c9c018b7866210f382386986662c27
Reinder de Vries <reinder@interactionauts.com>
2018-06-22 14:37:16 +0200

    Practice Hour #6 Conditionals:

---
+++ 6_Conditionals/6_Conditionals.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Practice Hour – https://learnappmaking.com/live
+
+import Foundation
+
+let color = "black"
+
+if color == "green" { }
+else if color == "yellow" { }
+else if color == "purple" { }
+else { }
+
+class Car
+{
+    func drive()
+    {
+        let speed = 100
+        
+    }
+}
+
+print(speed)
